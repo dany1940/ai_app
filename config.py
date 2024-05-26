338093f1f29ee08f88f80b281e35b3c8d881eca8
dany1940 <59851426+dany1940@users.noreply.github.com>
2024-05-26 14:58:03 +0100

    close #2

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+
+
+
+class Config:
+    postgres_host = os.getenv('postgres_host)
+    postgres_user = 'FlorinDumitrascu'
+    postgrest_port = 5432
+    postgres_db = 'teamcity
+    postgres_password = 'password'
+
